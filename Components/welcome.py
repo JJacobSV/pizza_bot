42f99296169fac51c0e1f1726b23986802fbd307
JJacobSV <17018@my.sanctamaria.school.nz>
2022-02-21 10:41:14 +1300

    Created component file and welcome file
    Welcome component created - version 1

---
+++ Components/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+# List of random names
+names = ["Alice", "Patrica", "Jeremy", "Micky", "Dees", "Lisa", "Veronica", "Mike", "Ox", "Slong"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welome to Dream Pizza ***")
+print("*** My name is", name, "***")
+print("*** I will be here to help you orderour delicious Dream Pizza ***")
--- pizza_bot.py
+++ pizza_bot.py
@@ -1,12 +1,1 @@
 #Pizza Bot Program
-import random
-from random import randint
-
-# List of random names
-names = ["Alice", "Patrica", "Jeremy", "Micky", "Dees", "Lisa", "Veronica", "Mike", "Ox", "Slong"]
-
-num = randint(0,9)
-
-name = (names[num])
-
-print(name)
