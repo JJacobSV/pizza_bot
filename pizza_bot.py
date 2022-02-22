e3739f39468d2b38dc97cf731ad15b6bc8b04ab0
JJacobSV <17018@my.sanctamaria.school.nz>
2022-02-23 12:16:15 +1300

    Added comments for each section
    comments for each section

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,1 +1,65 @@
-#Pizza Bot Program
+# Pizza Bot Program
+
+# Welcome message with random name
+
+
+
+
+
+
+# Menu for pickup or delivery
+
+
+
+
+
+
+# Pick up information - name and phone number
+
+
+
+
+
+
+# Delivery information - name address and phone
+
+
+
+
+
+# Choose total number of Pizzas - max 5
+
+
+
+
+
+
+# Pizza menu
+
+
+
+
+
+
+# Pizza order - from menu - print each pizza ordered with cost
+
+
+
+
+
+# Print order out - Including if order is delivery or pick up and names and price of each pizza - total cost including any deliveery charge
+
+
+
+
+# Ability to cancel or procced with order
+
+
+
+# Option for new order or to exit
+
+
+
+
+
+# Main function
