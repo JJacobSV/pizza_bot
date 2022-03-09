5dbd7de3e190cf86c2136913eeeb9456300a867f
JJacobSV <17018@my.sanctamaria.school.nz>
2022-03-09 15:12:11 +1300

    Create pickup v1.py

---
+++ Components/pickup v1.py
@@ -0,0 +1,15 @@
+#bug - accepts input
+
+print("Please enter the pickup information")
+
+#customer name
+name = input("Please enter your name, ")
+
+
+
+#customer phone number
+phone = input("Please enter your phone number, ")
+
+
+print(name)
+print(phone)
